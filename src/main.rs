7b055d5f898bc23d7438af39717ae581e7c03699
Dan Lock <codenil@proton.me>
2025-07-08 14:07:32 +0100

    Basic rendering demo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,108 @@
-use bevy::prelude::*;
+use bevy::{
+    core_pipeline::{bloom::Bloom, tonemapping::Tonemapping},
+    pbr::{
+        Atmosphere, AtmosphereSettings, CascadeShadowConfigBuilder, NotShadowCaster,
+        light_consts::lux,
+    },
+    prelude::*,
+    render::camera::Exposure,
+};
+use smooth_bevy_cameras::{
+    LookTransformPlugin,
+    controllers::unreal::{UnrealCameraBundle, UnrealCameraController, UnrealCameraPlugin},
+};
 
 fn main() {
-    App::new().run();
+    App::new()
+        .insert_resource(AmbientLight {
+            brightness: 0.2,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugins((LookTransformPlugin, UnrealCameraPlugin::default()))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Camera
+    commands
+        .spawn((
+            Camera3d::default(),
+            Camera {
+                hdr: true,
+                ..default()
+            },
+            Transform::from_xyz(-1.2, 0.15, 0.0).looking_at(Vec3::Y * 0.1, Vec3::Y),
+            Atmosphere::EARTH,
+            AtmosphereSettings {
+                aerial_view_lut_max_distance: 3.2e5,
+                scene_units_to_m: 1e+4,
+                ..Default::default()
+            },
+            Exposure::SUNLIGHT,
+            Tonemapping::AcesFitted,
+            Bloom::NATURAL,
+            DistanceFog {
+                color: Color::srgba(0.35, 0.48, 0.66, 1.0),
+                directional_light_color: Color::srgba(1.0, 0.95, 0.85, 0.5),
+                directional_light_exponent: 30.0,
+                falloff: FogFalloff::from_visibility_colors(
+                    15.0,
+                    Color::srgb(0.35, 0.5, 0.66),
+                    Color::srgb(0.8, 0.844, 1.0),
+                ),
+            },
+        ))
+        .insert(UnrealCameraBundle::new(
+            UnrealCameraController::default(),
+            Vec3::new(-2.0, 5.0, 5.0),
+            Vec3::new(0., 0., 0.),
+            Vec3::Y,
+        ));
+
+    // Circular base
+    commands.spawn((
+        Mesh3d(meshes.add(Circle::new(4.0))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+    ));
+    // Cube
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+
+    // Sun
+    commands.spawn((
+        DirectionalLight {
+            illuminance: lux::RAW_SUNLIGHT,
+            shadows_enabled: true,
+            ..default()
+        },
+        CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 0.3,
+            maximum_distance: 3.0,
+            ..default()
+        }
+        .build(),
+        Transform::from_xyz(1.0, -0.4, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+    // Sky
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(2.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Srgba::hex("888888").unwrap().into(),
+            unlit: true,
+            cull_mode: None,
+            ..default()
+        })),
+        Transform::from_scale(Vec3::splat(1_000_000.0)),
+        NotShadowCaster,
+    ));
 }
