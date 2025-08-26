530b8c55c7aaf5bb8f79a837cb2cc23cfdd03142
Dan Lock <codenil@proton.me>
2025-08-26 18:53:20 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(portable_simd, default_field_values)]
 
-use bevy::{app::plugin_group, prelude::*};
+use bevy::prelude::*;
 
 mod render;
 use crate::render::WorldRenderingPlugin;
@@ -12,38 +12,10 @@ use crate::camera::CameraPlugin;
 pub mod world;
 use crate::world::WorldPlugin;
 
-plugin_group! {
-    struct CustomPlugins {
-        bevy::app:::PanicHandlerPlugin,
-        bevy::log:::LogPlugin,
-        bevy::app:::TaskPoolPlugin,
-        bevy::diagnostic:::FrameCountPlugin,
-        bevy::time:::TimePlugin,
-        bevy::transform:::TransformPlugin,
-        bevy::diagnostic:::DiagnosticsPlugin,
-        bevy::input:::InputPlugin,
-        bevy::app:::ScheduleRunnerPlugin,
-        bevy::window:::WindowPlugin,
-        bevy::a11y:::AccessibilityPlugin,
-        bevy::app:::TerminalCtrlCHandlerPlugin,
-        bevy::asset:::AssetPlugin,
-        bevy::winit:::WinitPlugin,
-        bevy::render:::RenderPlugin,
-        bevy::image:::ImagePlugin,
-        bevy::render::pipelined_rendering:::PipelinedRenderingPlugin,
-        bevy::core_pipeline:::CorePipelinePlugin,
-        bevy::sprite:::SpritePlugin,
-        bevy::sprite_render:::SpriteRenderingPlugin,
-        bevy::text:::TextPlugin,
-        bevy::ui:::UiPlugin,
-        bevy::ui_render:::UiRenderPlugin,
-    }
-}
-
 fn main() {
     App::new()
         .add_plugins((
-            CustomPlugins,
+            DefaultPlugins,
             WorldPlugin,
             CameraPlugin,
             WorldRenderingPlugin,
